8296f5b07194b114234dd6babd23a22be79530a1
tekofx <tekofxx@gmail.com>
2025-05-15 15:04:42 +0200

    Hyprland settings

--- modules/home-manager/desktop/hypr/hyprland.nix
+++ modules/home-manager/desktop/hypr/hyprland.nix
@@ -1,11 +1,175 @@
-{ ... }: {
+{ lib, config, ... }: {
 
   home.stateVersion = "24.11";
 
   imports = [
 
-    ./hyprpanel.nix 
+    ./hyprpanel.nix
     ./wofi.nix
   ];
+  wayland.windowManager.hyprland.enable = true;
+
+  wayland.windowManager.hyprland.settings = {
+
+    ############################### Prefered programs ###############################
+    "$terminal" = "kitty";
+    "$files" = "nautilus";
+    "$menu" = "wofi --show drun";
+
+    monitor = ",preferred,auto,1";
+
+    ############################### Exec once ###############################
+    exec-once = [
+      "hyprpaper"
+      "hyprpanel"
+
+    ];
+
+    ############################### General, Decoration and Animation ###############################
+
+    general = {
+      gaps_in = 5;
+      gaps_out = 2;
+
+      border_size = 1;
+
+      # https://wiki.hyprland.org/Configuring/Variables/#variable-types for info about colors
+      /* "col.active_border" = lib.mkForce
+           "rgba(${config.stylix.base16Scheme.base0E}ff) rgba(${config.stylix.base16Scheme.base09}ff) 60deg";
+         "col.inactive_border" =
+           lib.mkForce "rgba(${config.stylix.base16Scheme.base00}ff)";
+      */
+      # Set to true enable resizing windows by clicking and dragging on borders and gaps
+      resize_on_border = false;
+
+      # Please see https://wiki.hyprland.org/Configuring/Tearing/ before you turn this on
+      allow_tearing = false;
+
+      layout = "dwindle";
+    };
+
+    decoration = {
+      rounding = 10;
+
+      active_opacity = 1.0;
+      inactive_opacity = 1.0;
+
+      shadow = {
+        enabled = true;
+        range = 4;
+        render_power = 3;
+      };
+
+      blur = {
+        enabled = true;
+        size = 3;
+        passes = 1;
+        vibrancy = 0.1696;
+      };
+    };
+
+    animations = {
+      enabled = "yes, please :)";
+
+      # Default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
+
+      bezier = [
+        "easeOutQuint,0.23,1,0.32,1"
+        "easeInOutCubic,0.65,0.05,0.36,1"
+        "linear,0,0,1,1"
+        "almostLinear,0.5,0.5,0.75,1.0"
+        "quick,0.15,0,0.1,1"
+      ];
+
+      animation = [
+        "global, 1, 10, default"
+        "border, 1, 5.39, easeOutQuint"
+        "windows, 1, 4.79, easeOutQuint"
+        "windowsIn, 1, 4.1, easeOutQuint, popin 87%"
+        "windowsOut, 1, 1.49, linear, popin 87%"
+        "fadeIn, 1, 1.73, almostLinear"
+        "fadeOut, 1, 1.46, almostLinear"
+        "fade, 1, 3.03, quick"
+        "layers, 1, 3.81, easeOutQuint"
+        "layersIn, 1, 4, easeOutQuint, fade"
+        "layersOut, 1, 1.5, linear, fade"
+        "fadeLayersIn, 1, 1.79, almostLinear"
+        "fadeLayersOut, 1, 1.39, almostLinear"
+        "workspaces, 1, 1.94, almostLinear, fade"
+        "workspacesIn, 1, 1.21, almostLinear, fade"
+        "workspacesOut, 1, 1.94, almostLinear, fade"
+      ];
+    };
+
+    ################################# Binds #################################
+
+    "$mainMod" = "SUPER";
+    bind = [
+      "$mainMod, return, exec, $terminal"
+      "$mainMod, Q, killactive,"
+      "$mainMod, Space, exec, wofi --show drun"
+      "$mainMod, M, exit,"
+      "$mainMod, E, exec, $fileManager"
+      "$mainMod, V, togglefloating,"
+      "$mainMod, R, exec, $menu"
+      "$mainMod, P, pseudo," # dwindle
+      "$mainMod, J, togglesplit," # dwindle
+      # Fullscreen
+      "$mainMod,F,fullscreen"
+
+      # Move focus with mainMod + arrow keys
+      "$mainMod, left, movefocus, l"
+      "$mainMod, right, movefocus, r"
+      "$mainMod, up, movefocus, u"
+      "$mainMod, down, movefocus, d"
+
+      # Switch workspaces with mainMod + [0-9]
+      "$mainMod, 1, workspace, 1"
+      "$mainMod, 2, workspace, 2"
+      "$mainMod, 3, workspace, 3"
+      "$mainMod, 4, workspace, 4"
+      "$mainMod, 5, workspace, 5"
+      "$mainMod, 6, workspace, 6"
+      "$mainMod, 7, workspace, 7"
+      "$mainMod, 8, workspace, 8"
+      "$mainMod, 9, workspace, 9"
+      "$mainMod, 0, workspace, 10"
+
+      # Move active window to a workspace with mainMod + SHIFT + [0-9]
+      "$mainMod SHIFT, 1, movetoworkspace, 1"
+      "$mainMod SHIFT, 2, movetoworkspace, 2"
+      "$mainMod SHIFT, 3, movetoworkspace, 3"
+      "$mainMod SHIFT, 4, movetoworkspace, 4"
+      "$mainMod SHIFT, 5, movetoworkspace, 5"
+      "$mainMod SHIFT, 6, movetoworkspace, 6"
+      "$mainMod SHIFT, 7, movetoworkspace, 7"
+      "$mainMod SHIFT, 8, movetoworkspace, 8"
+      "$mainMod SHIFT, 9, movetoworkspace, 9"
+      "$mainMod SHIFT, 0, movetoworkspace, 10"
+
+      # Scroll through existing workspaces with mainMod + scroll
+      "$mainMod, mouse_down, workspace, e+1"
+      "$mainMod, mouse_up, workspace, e-1"
+
+    ];
+
+    input = {
+      kb_layout = "es";
+      kb_variant = "";
+      kb_model = "";
+      kb_options = "";
+      kb_rules = "";
+
+      follow_mouse = 1;
+
+      touchpad = { natural_scroll = false; };
+
+      sensitivity = 0.0; # -1.0 - 1.0, 0 means no modification.
+    };
+    gestures = { workspace_swipe = true; };
+
+    xwayland = { force_zero_scaling = true; };
+
+  };
 
 }
