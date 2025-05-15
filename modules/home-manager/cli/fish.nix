6aa97b7d3e2708def2d0b2b32ce7dbc0be6d83fb
tekofx <tekofxx@gmail.com>
2025-05-15 15:20:52 +0200

    Added fzf options

--- modules/home-manager/cli/fish.nix
+++ modules/home-manager/cli/fish.nix
@@ -7,6 +7,18 @@
     enable = true;
     interactiveShellInit = ''
       set fish_greeting # Disable greeting
+      export FZF_DEFAULT_OPTS="
+        --ansi
+        --info=inline
+        --height=70%
+        --layout=reverse 
+        --preview-window='right:hidden:wrap:60%'
+        --bind='f2:toggle-preview'
+        --bind='alt-w:preview-page-up'
+        --bind='alt-s:preview-page-down'
+        --preview 'bat --color=always --style=header,grid --line-range :300 {}'
+      "
+
     '';
 
     shellAliases = {
