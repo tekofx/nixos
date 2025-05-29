8f827e9e3800639c3f68ee8ecf0a862af0d7a439
tekofx <tekofxx@gmail.com>
2025-05-29 19:36:38 +0200

    Removed windows settings

--- modules/global.nix
+++ modules/global.nix
@@ -26,13 +26,6 @@
     LC_TIME = "es_ES.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
   # Configure console keymap
   console.keyMap = "es";
 
