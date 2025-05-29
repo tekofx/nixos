c10c0b9f06686860c7c1f8477b92d064158a56a0
tekofx <tekofxx@gmail.com>
2025-05-29 16:28:13 +0000

    Added stateversion

--- modules/home-manager/home-manager.nix
+++ modules/home-manager/home-manager.nix
@@ -1,11 +1,13 @@
 { lib, pkgs, config, ... }: {
 
+  home.stateVersion = "25.05";
+
   imports = [
-    ./desktop/hypr/hyprland.nix
-    ./desktop/gnome.nix
-    ./kitty.nix
-    ./cli/fish.nix
-    ./cli/git.nix
+    #./desktop/hypr/hyprland.nix
+    #./desktop/gnome.nix
+    #./kitty.nix
+    #./cli/fish.nix
+    #./cli/git.nix
   ];
 
 }
