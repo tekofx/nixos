da1bde73c21795daf40a0d1dca6dcfd5d7bb1001
tekofx <tekofxx@gmail.com>
2025-05-29 17:10:26 +0000

    Reimported modules

--- modules/home-manager/home-manager.nix
+++ modules/home-manager/home-manager.nix
@@ -1,13 +1,13 @@
-{ lib, pkgs, config, ... }: {
+{ lib, pkgs, config, home-manager, ... }: {
 
   home.stateVersion = "25.05";
 
   imports = [
-    #./desktop/hypr/hyprland.nix
-    #./desktop/gnome.nix
-    #./kitty.nix
-    #./cli/fish.nix
-    #./cli/git.nix
+    ./desktop/hypr/hyprland.nix
+    ./desktop/gnome.nix
+    ./kitty.nix
+    ./cli/fish.nix
+    ./cli/git.nix
   ];
 
 }
