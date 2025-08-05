6adb69b113f0947eee133ee8b7d1094ccfac8676
tekofx <tekofxx@gmail.com>
2025-08-05 14:37:11 +0200

    Added go and vscode

--- modules/nixos/development.nix
+++ modules/nixos/development.nix
@@ -1,15 +1,17 @@
-# btop install
 { pkgs, ... }: {
+
+  nix.settings.trusted-users = [ "root" "teko" ];
 
   environment.systemPackages = with pkgs; [
     zed-editor
     android-studio
+    vscode
+    go
     bruno
     nil
     nerd-fonts.roboto-mono
     devenv
     direnv
-
   ];
 
   services.flatpak.packages =
